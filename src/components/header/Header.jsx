b2f972304a835f88510f72537afcae3f9d46ad5c
ui3 <sina.saeedifar@gmail.com>
2022-02-27 18:33:11 +0330

    adding CTA and some header codes . . .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,8 +3,12 @@ import './header.css'
 
 const Header = () => {
     return (
-        <div>Header</div>
-    )
+        <header>
+            <div className='conatiner header__container'>
+                <h5>Hello I'm</h5>
+                <h1>Sina Saeedifar</h1>
+                <h5 className='text-light'>Front-End Developer</h5>
+            </div></header>)
 }
 
 export default Header
